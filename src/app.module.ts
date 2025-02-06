dc638831b5a67e24b66952ace1b8eed90d861117
Kasra <amirkasrakazemi@gmail.com>
2025-02-06 09:11:38 +0330

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://admin:1234@localhost:27017/main_db'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
